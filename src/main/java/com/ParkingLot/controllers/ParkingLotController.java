cb15764276a6d7afc79c8e6fd24aa8e20e2fccdb
Bhaskar Bhakat <bhaskarbhakat40@gmail.com>
2023-05-27 02:44:02 +0530

    parking lot controller

---
+++ src/main/java/com/ParkingLot/controllers/ParkingLotController.java
@@ -0,0 +1,9 @@
+package com.ParkingLot.controllers;
+
+public class ParkingLotController {
+    
+    // 1. Create Parking Lot
+    // 2. Update Parking Lot
+    // 3. Read Parking Lot
+    // 4. Delete Parking Lot
+}
---
+++ src/main/java/com/ParkingLot/model/Ticket.java
@@ -0,0 +1,5 @@
+package com.ParkingLot;
+
+public class Ticket {
+    
+}
